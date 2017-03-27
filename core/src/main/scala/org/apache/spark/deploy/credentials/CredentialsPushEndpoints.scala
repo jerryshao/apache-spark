8cac23b092b83ac1a29bfb53c254a200e2ec4b5c
jerryshao <sshao@hortonworks.com>
2017-03-27 21:12:33 +0800

    Refactor the push endpoint
    Change-Id: I413203e1ea69ed81d9257a85d63ff490aed3e59e

--- core/src/main/scala/org/apache/spark/deploy/credentials/CredentialsPushEndpoints.scala
+++ core/src/main/scala/org/apache/spark/deploy/credentials/CredentialsPushEndpoints.scala
@@ -23,21 +23,26 @@ import scala.collection.JavaConverters._
 import scala.concurrent.Future
 import scala.util.control.NonFatal
 
-import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.io.DataOutputBuffer
 import org.apache.hadoop.security.{Credentials, UserGroupInformation}
 
 import org.apache.spark.{SecurityManager, SparkConf, SparkContext, SparkException}
+import org.apache.spark.deploy.SparkHadoopUtil
 import org.apache.spark.internal.Logging
 import org.apache.spark.rpc._
 import org.apache.spark.scheduler.cluster.CoarseGrainedClusterMessages.UpdateCredentials
 import org.apache.spark.scheduler.cluster.CoarseGrainedSchedulerBackend
 import org.apache.spark.util.{ThreadUtils, Utils}
 
-private[spark] class CredentialsUpdateEndpoint(override val rpcEnv: RpcEnv)
-  extends RpcEndpoint with Logging {
+/**
+ * A [[CredentialsPushEndpoint]] receives [[UpdateCredentials]] from remote endpoint and updates
+ * credentials in Driver side UGI, also propagates this credentials to all the remote Executors
+ * and AM (in yarn mode). The whole RPC propagation is chained by Scala Future, user could wait
+ * on the Future until all the updates after finished successfully.
+ */
+private[spark] class CredentialsPushEndpoint(override val rpcEnv: RpcEnv, sc: SparkContext)
+    extends RpcEndpoint with Logging {
   private implicit val executor = ThreadUtils.sameThread
-  private lazy val sc = SparkContext.getOrCreate()
 
   override def receiveAndReply(context: RpcCallContext): PartialFunction[Any, Unit] = {
     case UpdateCredentials(c) =>
@@ -48,7 +53,7 @@ private[spark] class CredentialsUpdateEndpoint(override val rpcEnv: RpcEnv)
               val dataInput = new DataInputStream(new ByteArrayInputStream(c))
               val credentials = new Credentials
               credentials.readFields(dataInput)
-              logInfo(s"Update credentials with Tokens " +
+              logInfo("Update credentials with Tokens " +
                 s"${credentials.getAllTokens.asScala.map(_.getKind.toString).mkString(",")} " +
                 "in driver")
               UserGroupInformation.getCurrentUser.addCredentials(credentials)
@@ -64,34 +69,41 @@ private[spark] class CredentialsUpdateEndpoint(override val rpcEnv: RpcEnv)
             future onFailure {
               case NonFatal(e) => context.sendFailure(e)
             }
+
           case _ =>
-            throw new SparkException(s"Update credentials on" +
+            throw new SparkException("Update credentials on" +
               s" ${sc.schedulerBackend.getClass.getSimpleName} is not supported")
         }
   }
 }
 
-private[spark] object CredentialsUpdateEndpoint {
+private[spark] object CredentialsPushEndpoint {
   val ENDPOINT_NAME = "CREDENTIALS_ENDPOINT"
 }
 
-private[deploy] abstract class CredentialsUpdateEndpointRef(
-    applicationId: String, rpcEnv: RpcEnv) {
+/**
+ * A [[CredentialsPushEndpointRef]] used to connect to [[CredentialsPushEndpoint]] and push
+ * credentials to the endpoint. Each cluster manager should implement its own way to provide
+ * driver's RPC host and port to connect to.
+ */
+private[deploy] abstract class CredentialsPushEndpointRef(
+    sparkConf: SparkConf,
+    applicationId: String) {
+
   def driverHost: String
   def driverPort: Int
 
-  def endpointRef: RpcEndpointRef = {
-    rpcEnv.setupEndpointRef(RpcAddress(driverHost, driverPort),
-      CredentialsUpdateEndpoint.ENDPOINT_NAME)
-  }
-}
-
-class CredentialsUtils(sparkConf: SparkConf) {
-  private val hadoopConf = new Configuration()
+  private val hadoopConf = SparkHadoopUtil.get.conf
   private val securityManager = new SecurityManager(sparkConf)
+  private lazy val credentialMgr = new ConfigurableCredentialManager(sparkConf, hadoopConf)
+
   private val rpcEnv = RpcEnv.create(
-    "sparkLauncher", Utils.localHostName(), 0, sparkConf, securityManager, clientMode = true)
-  private val credentialMgr = new ConfigurableCredentialManager(sparkConf, hadoopConf)
+      "sparkLauncher", Utils.localHostName(), 0, sparkConf, securityManager, clientMode = true)
+
+  private def endpointRef(): RpcEndpointRef = {
+    rpcEnv.setupEndpointRef(RpcAddress(driverHost, driverPort),
+      CredentialsPushEndpoint.ENDPOINT_NAME)
+  }
 
   def updateCredentials(applicationId: String): Future[Boolean] = {
     val credentials = new Credentials()
@@ -102,18 +114,18 @@ class CredentialsUtils(sparkConf: SparkConf) {
   def updateCredentials(applicationId: String, credentials: Credentials): Future[Boolean] = {
     val dob = new DataOutputBuffer()
     credentials.write(dob)
-
-    val endpointRef = createEndpointRef(applicationId)
-    endpointRef.ask[Boolean](UpdateCredentials(dob.getData))
+    endpointRef().ask[Boolean](UpdateCredentials(dob.getData))
   }
+}
 
-  private def createEndpointRef(applicationId: String): RpcEndpointRef = {
-    val credentialsUpdateEndpointRef = if (sparkConf.get("spark.master").startsWith("yarn")) {
+object CredentialsPushEndpointRef {
+  def createEndpointRef(applicationId: String, sparkConf: SparkConf): CredentialsPushEndpointRef = {
+    if (sparkConf.get("spark.master").startsWith("yarn")) {
       try {
         Utils.classForName("org.apache.spark.deploy.yarn.YarnCredentialsUpdateEndpointRef")
-          .getConstructor(classOf[String], classOf[RpcEnv])
-          .newInstance(applicationId, rpcEnv)
-          .asInstanceOf[CredentialsUpdateEndpointRef]
+          .getConstructor(classOf[String], classOf[SparkConf])
+          .newInstance(applicationId, sparkConf)
+          .asInstanceOf[CredentialsPushEndpointRef]
       } catch {
         case NonFatal(e) =>
           throw new SparkException("Failed to create CredentialsUpdateEndpointRef", e)
@@ -122,7 +134,5 @@ class CredentialsUtils(sparkConf: SparkConf) {
       throw new SparkException(s"Credentials update is not supported for " +
         s"${sparkConf.get("spark.master")} cluster manager")
     }
-
-    credentialsUpdateEndpointRef.endpointRef
   }
 }
